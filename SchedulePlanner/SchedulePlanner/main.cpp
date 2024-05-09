66e46289820440195eedc509040f952a4a8dd7a9
J-Abitria <j.abitria@wsu.edu>
2024-05-08 21:59:33 -0700

    Added a project (WIP) for a schedule planner.

---
+++ SchedulePlanner/SchedulePlanner/main.cpp
@@ -0,0 +1,14 @@
+/*
+	Created by: James Abitria
+	Project Name: Schedule Planner
+	Date Created: 5-8-24
+	Last Update: 5-8-24
+	
+	Description: This program runs a schedule planner that can be loaded or saved at any time at the user's discretion. It runs a menu that
+	lets a user assign an event to each day of the week, at any of the 24 hours of the day, in military time.
+*/
+
+int main() {
+
+	return 0;
+}
