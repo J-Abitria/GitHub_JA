dcef8b7dcf037cbdaaf452e2a486d0226ef7ad42
J-Abitria <j.abitria@wsu.edu>
2024-05-13 17:55:06 -0700

    Created a new project StocksSimulator.

---
+++ StocksSimulator/StocksSimulator/main.cpp
@@ -0,0 +1,14 @@
+/*
+	Created by: James Abitria
+	Project Name: Stocks Simulator
+	Date Created: 5-13-24
+	Last Update: 5-13-24
+	
+	Description: This program runs a "game" where the user can choose to buy or sell stocks based on their current values. Certain stocks
+	have more volatility than others, and can be bought in higher or lower amounts.
+*/
+
+int main() {
+
+	return 0;
+}
